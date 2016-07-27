8da0a93adced784bbb625929a0cec720b4d6d2f1
jlculbreath <jeremy.culbreath@gmail.com>
2016-07-26 20:42:58 -0500

    Add files via upload

---
+++ morse.h
@@ -0,0 +1,21 @@
+#pragma once
+//
+//  CS303
+//  Project 3B
+//
+//  Lucile Kull, John Buckley, Jeremy Culbreath
+//
+//	morse.h
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include "build_bTree.h"
+#include "morse.h"
+using namespace std;
+
+void build_tree(string file, morse_code& tree);
+map <char, string> populateMap(); //Populate map for english char and morse code eqivilent
+string encodeMessage(map <char, string> map, string message); //Encode message using morse code
+string decodeMessage(morse_code tree, string message); //Decode message from morse code into plain text
