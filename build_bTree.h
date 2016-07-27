d3ae58a3c384a382f51f04e1738a2b6dbe8bef9f
Lucy Kull <lucy.r.kull@gmail.com>
2016-07-27 00:36:05 -0500

    Add files via upload

--- build_bTree.h
+++ build_bTree.h
@@ -14,15 +14,39 @@ using namespace std;
 struct letter  //node for tree
 {
 public:
+<<<<<<< HEAD
+    char key; //characters
+    vector<char> code;  //morse code vector
+    struct letter *left;  //pointers to continue tree
+    struct letter *right;
+=======
 	char key; //characters
 	vector<char> code;  //morse code vector
 	struct letter *left;  //pointers to continue tree
 	struct letter *right;
+>>>>>>> origin/master
 };
 
 class morse_code
 {
 public:
+<<<<<<< HEAD
+    morse_code();  //constructor
+    
+    
+    //these functions are for easy use and access in the main function, they call a more complicated version
+    void insert(char key, vector<char> code);  //function to add to tree
+    letter* search(vector<char> code); //function to find code/character combo when given a code
+    
+    //these are the more complicated versions
+    void insert(char key, vector<char> code, struct letter *leaf); //insert node starting at given node
+    letter* search(vector<char> code, letter *leaf); //search starting at given node
+    friend bool operator > (const vector<char>& other, const vector<char>& rhs); //overloaded > operator for search and insert
+    friend bool operator < (const vector<char>& other, const vector<char>& rhs); //overloaded < operator for search and insert
+    friend bool operator == (const vector<char>& other, const vector<char>& rhs); //overloaded == operator for search and insert
+    
+    letter *root;  //root of tree
+=======
 	morse_code();  //constructor
 	
 
@@ -38,6 +62,7 @@ public:
 	friend bool operator == (const vector<char>& other, const vector<char>& rhs); //overloaded == operator for search and insert
 
 	letter *root;  //root of tree
+>>>>>>> origin/master
 };
 
 #endif /* build_bTree_hpp */
